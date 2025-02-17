32de324bc55d79d9309e5de38d5a77ea7a76b854
Alexander Stepanov <alexanderlawyer97@gmail.com>
2025-02-18 01:17:11 +0400

    Initial Commit

---
+++ Daily Planner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Daily Planner
+//
+//  Created by Mac Alexander on 18.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
